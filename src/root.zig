559509ddd69c8812714bc93102a92c92a9ec8d82
Zoee Silcock <mrzoee@gmail.com>
2024-12-09 15:42:38 +0100

    Add an example sprite

--- src/root.zig
+++ src/root.zig
@@ -11,27 +11,15 @@ pub const State = struct {
     world_width: u32,
     world_height: u32,
 
-    render_texture: ?r.RenderTexture2D = null,
-    source_rect: ?r.Rectangle = null,
-    dest_rect: ?r.Rectangle = null,
+    render_texture: ?r.RenderTexture2D,
+    source_rect: ?r.Rectangle,
+    dest_rect: ?r.Rectangle,
 
-    pub fn initializeRenderTexture(self: *State) void {
-        self.world_width = @intFromFloat(@divFloor(@as(f32, @floatFromInt(self.window_width)), self.world_scale));
-        self.world_height = @intFromFloat(@divFloor(@as(f32, @floatFromInt(self.window_height)), self.world_scale));
-        self.render_texture = r.LoadRenderTexture(@intCast(self.world_width), @intCast(self.world_height));
-        self.source_rect = r.Rectangle{
-            .x = 0,
-            .y = 0,
-            .width = @floatFromInt(self.render_texture.?.texture.width),
-            .height = @floatFromInt(self.render_texture.?.texture.height),
-        };
-        self.dest_rect = r.Rectangle{ 
-            .x = @as(f32, @floatFromInt(self.window_width)) - self.source_rect.?.width * self.world_scale,
-            .y = 0,
-            .width = self.source_rect.?.width * self.world_scale,
-            .height = self.source_rect.?.height * self.world_scale,
-        };
-    }
+    assets: Assets,
+};
+
+const Assets = struct {
+    test_texture: ?r.Texture2D,
 };
 
 export fn init(window_width: u32, window_height: u32) *anyopaque {
@@ -39,15 +27,22 @@ export fn init(window_width: u32, window_height: u32) *anyopaque {
     var state: *State = allocator.create(State) catch @panic("Out of memory");
 
     state.allocator = allocator;
+
     state.window_width = window_width;
     state.window_height = window_height;
+    state.render_texture = null;
+    state.source_rect = null;
+    state.dest_rect = null;
     state.world_scale = 4;
+
+    loadAssets(state);
 
     return state;
 }
 
 export fn reload(state_ptr: *anyopaque) void {
-    _ = state_ptr;
+    const state: *State = @ptrCast(@alignCast(state_ptr));
+    loadAssets(state);
 }
 
 export fn tick(state_ptr: *anyopaque) void {
@@ -55,10 +50,10 @@ export fn tick(state_ptr: *anyopaque) void {
 }
 
 export fn draw(state_ptr: *anyopaque) void {
-    var state: *State = @ptrCast(@alignCast(state_ptr));
+    const state: *State = @ptrCast(@alignCast(state_ptr));
 
     if (state.render_texture == null) {
-        state.initializeRenderTexture();
+        initializeRenderTexture(state);
     }
 
     if (state.render_texture) |render_texture| {
@@ -76,11 +71,36 @@ export fn draw(state_ptr: *anyopaque) void {
     }
 }
 
-fn drawWorld(state: *State) void {
-    const size = r.Vector2{ .x = 30, .y = 20 };
-    const position = r.Vector2{
-        .x = @as(f32, @floatFromInt(state.world_width)) / 2 - size.x / 2,
-        .y = @as(f32, @floatFromInt(state.world_height)) / 2 - size.y / 2,
+fn initializeRenderTexture(state: *State) void {
+    state.world_width = @intFromFloat(@divFloor(@as(f32, @floatFromInt(state.window_width)), state.world_scale));
+    state.world_height = @intFromFloat(@divFloor(@as(f32, @floatFromInt(state.window_height)), state.world_scale));
+    state.render_texture = r.LoadRenderTexture(@intCast(state.world_width), @intCast(state.world_height));
+    state.source_rect = r.Rectangle{
+        .x = 0,
+        .y = 0,
+        .width = @floatFromInt(state.render_texture.?.texture.width),
+        .height = -@as(f32, @floatFromInt(state.render_texture.?.texture.height)),
     };
-    r.DrawRectangleV(position, size, r.RED);
+    state.dest_rect = r.Rectangle{ 
+        .x = @as(f32, @floatFromInt(state.window_width)) - state.source_rect.?.width * state.world_scale,
+        .y = 0,
+        .width = state.source_rect.?.width * state.world_scale,
+        .height = state.source_rect.?.height * state.world_scale,
+    };
+}
+
+fn loadAssets(state: *State) void {
+    state.assets.test_texture = r.LoadTexture("assets/test.png");
+}
+
+fn drawWorld(state: *State) void {
+    if (state.assets.test_texture) |sprite| {
+        const size = r.Vector2{ .x = @floatFromInt(sprite.width), .y = @floatFromInt(sprite.height) };
+        const position = r.Vector2{
+            .x = @as(f32, @floatFromInt(state.world_width)) / 2 - size.x / 2,
+            .y = @as(f32, @floatFromInt(state.world_height)) / 2 - size.y / 2,
+        };
+
+        r.DrawTextureV(sprite, position, r.WHITE);
+    }
 }
