700f2e7fe0845316e9f9447fd26ec69a4037e8f0
Zoee Silcock <mrzoee@gmail.com>
2026-03-21 16:05:17 +0100

    Replace "template" and "Template" with the new project name when generating new projects based on the template

--- src/new.zig
+++ src/new.zig
@@ -8,53 +8,11 @@ const IGNORED_PATHS = [_][]const u8{
     "zig-out",
 };
 
-fn isPathIgnored(path: []const u8) bool {
-    var result: bool = false;
-    for (IGNORED_PATHS) |ignored_path| {
-        if (std.mem.eql(u8, path, ignored_path)) {
-            result = true;
-            break;
-        }
-    }
-    return result;
-}
-
-fn copyDirectory(
-    source_dir: std.fs.Dir,
-    target_dir: std.fs.Dir,
-    stdout: *std.Io.Writer,
-    allocator: std.mem.Allocator,
-    level: u32,
-) !void {
-    var walker = try source_dir.walk(allocator);
-    defer walker.deinit();
-
-    // TODO: This only looks nice up to one level deep. Rebuild this to support deeper levels when we need it.
-    const prefix: []const u8 = if (level == 0) "├─ " else "│  ├─";
-    const end_prefix: []const u8 = if (level == 0) "└─ " else "│  └─ ";
-
-    while (try walker.next()) |entry| {
-        if (!isPathIgnored(entry.path) and entry.dir.fd == source_dir.fd) {
-            if (entry.kind == .file) {
-                try stdout.print("{s}Copying file: {s}.\n", .{ prefix, entry.path });
-                try entry.dir.copyFile(entry.path, target_dir, entry.path, .{});
-            } else if (entry.kind == .directory) {
-                try stdout.print("{s}Creating directory: {s}.\n", .{ prefix, entry.path });
-                try target_dir.makeDir(entry.path);
-
-                var source_sub_dir: std.fs.Dir = try source_dir.openDir(entry.path, .{ .access_sub_paths = false });
-                defer source_sub_dir.close();
-
-                var target_sub_dir: std.fs.Dir = try target_dir.openDir(entry.path, .{ .access_sub_paths = false });
-                defer target_sub_dir.close();
-
-                try copyDirectory(source_sub_dir, target_sub_dir, stdout, allocator, level + 1);
-            }
-        }
-    }
-
-    try stdout.print("{s}Done.\n", .{end_prefix});
-}
+const FILES_WITH_SUBSTITUTIONS = [_][]const u8{
+    "root.zig",
+    "build.zig",
+    "build.zig.zon",
+};
 
 pub fn main() !void {
     var stdout_buffer: [1024]u8 = undefined;
@@ -94,10 +52,12 @@ pub fn main() !void {
         try std.fs.cwd().makePath(target_path);
         target_dir = try std.fs.cwd().openDir(target_path, .{ .access_sub_paths = false });
 
+        const new_name: []const u8 = std.fs.path.basename(target_path);
+
         // Copy template files to target directory.
         var source_dir = try std.fs.cwd().openDir(source_path, .{ .access_sub_paths = false, .iterate = true });
         defer source_dir.close();
-        try copyDirectory(source_dir, target_dir.?, stdout, allocator, 0);
+        try copyDirectory(source_dir, target_dir.?, new_name, stdout, allocator, 0);
 
         try stdout.print("\nYou're all setup!\n\n", .{});
         try stdout.print("Run your new project:\n`cd {s} && zig build run`\n\n", .{target_path});
@@ -107,4 +67,180 @@ pub fn main() !void {
     }
 
     try stdout.flush(); // Don't forget to flush!
+}
+
+fn copyDirectory(
+    source_dir: std.fs.Dir,
+    target_dir: std.fs.Dir,
+    new_name: []const u8,
+    stdout: *std.Io.Writer,
+    allocator: std.mem.Allocator,
+    level: u32,
+) !void {
+    var walker = try source_dir.walk(allocator);
+    defer walker.deinit();
+
+    // TODO: This only looks nice up to one level deep. Rebuild this to support deeper levels when we need it.
+    const prefix: []const u8 = if (level == 0) "├─ " else "│  ├─ ";
+    const end_prefix: []const u8 = if (level == 0) "└─ " else "│  └─ ";
+
+    while (try walker.next()) |entry| {
+        if (!isPathIgnored(entry.path) and entry.dir.fd == source_dir.fd) {
+            if (entry.kind == .file) {
+                try stdout.print("{s}Copying file: {s}.\n", .{ prefix, entry.path });
+                if (fileHasSubstitutions(entry.path)) {
+                    try copyFileWithSubstitutions(source_dir, target_dir, entry.path, new_name);
+                } else {
+                    try entry.dir.copyFile(entry.path, target_dir, entry.path, .{});
+                }
+            } else if (entry.kind == .directory) {
+                try stdout.print("{s}Creating directory: {s}.\n", .{ prefix, entry.path });
+                try target_dir.makeDir(entry.path);
+
+                var source_sub_dir: std.fs.Dir = try source_dir.openDir(entry.path, .{ .access_sub_paths = false });
+                defer source_sub_dir.close();
+
+                var target_sub_dir: std.fs.Dir = try target_dir.openDir(entry.path, .{ .access_sub_paths = false });
+                defer target_sub_dir.close();
+
+                try copyDirectory(source_sub_dir, target_sub_dir, new_name, stdout, allocator, level + 1);
+            }
+        }
+    }
+
+    try stdout.print("{s}Done.\n", .{end_prefix});
+}
+
+fn copyFileWithSubstitutions(
+    source_dir: std.fs.Dir,
+    target_dir: std.fs.Dir,
+    file_name: []const u8,
+    new_name: []const u8,
+) !void {
+    const source_file: std.fs.File = try source_dir.openFile(file_name, .{ .mode = .read_only });
+    defer source_file.close();
+    var read_buffer: [1024]u8 = undefined;
+    var file_reader = source_file.reader(&read_buffer);
+    const reader: *std.Io.Reader = &file_reader.interface;
+
+    const dest_file: std.fs.File = try target_dir.createFile(file_name, .{});
+    defer dest_file.close();
+    var write_buffer: [1024]u8 = undefined;
+    var file_writer = dest_file.writer(&write_buffer);
+    const writer = &file_writer.interface;
+
+    var string_buffer: [1024]u8 = undefined;
+    var string_length: u32 = 0;
+    var string_discarding_writer: std.Io.Writer.Discarding = .init(&string_buffer);
+    const string_writer = &string_discarding_writer.writer;
+
+    while (true) {
+        const next_character = reader.take(1) catch break;
+        try writer.print("{s}", .{next_character});
+
+        if (std.mem.eql(u8, next_character, "\"")) {
+            string_length = 0;
+            try string_writer.flush();
+
+            while (true) {
+                const peek = reader.peek(1) catch break;
+
+                if (std.mem.eql(u8, peek, "\"")) {
+                    const string = string_buffer[0..string_length];
+                    try printStringOrSubstitute(string, writer, new_name);
+                    break;
+                } else {
+                    reader.toss(1);
+                    try string_writer.print("{s}", .{peek});
+                    string_length += 1;
+                }
+            }
+
+            _ = reader.stream(writer, .limited(1)) catch break;
+        } else if (std.mem.eql(u8, next_character, ".")) {
+            string_length = 0;
+            try string_writer.flush();
+
+            while (true) {
+                const peek = reader.peek(1) catch break;
+
+                if (std.mem.eql(u8, peek, " ") or
+                    std.mem.eql(u8, peek, ".") or
+                    std.mem.eql(u8, peek, "(") or
+                    std.mem.eql(u8, peek, ")") or
+                    std.mem.eql(u8, peek, ",") or
+                    std.mem.eql(u8, peek, "{"))
+                {
+                    const string = string_buffer[0..string_length];
+                    try printStringOrSubstitute(string, writer, new_name);
+                    break;
+                } else {
+                    reader.toss(1);
+                    try string_writer.print("{s}", .{peek});
+                    string_length += 1;
+                }
+            }
+
+            _ = reader.stream(writer, .limited(1)) catch break;
+        }
+    }
+
+    try writer.flush();
+}
+
+fn printStringOrSubstitute(
+    string: []const u8,
+    writer: *std.Io.Writer,
+    new_name: []const u8,
+) !void {
+    if (std.mem.eql(u8, string, "template")) {
+        try writer.print("{s}", .{new_name});
+    } else if (std.mem.eql(u8, string, "Template")) {
+        try printCapitalizedName(new_name, writer);
+    } else {
+        try writer.print("{s}", .{string});
+    }
+}
+
+fn printCapitalizedName(name: []const u8, writer: *std.Io.Writer) !void {
+    var reader: std.Io.Reader = std.Io.Reader.fixed(name);
+    var capitalize: bool = true;
+
+    while (true) {
+        var next_character = reader.take(1) catch break;
+
+        if (capitalize) {
+            capitalize = false;
+            next_character[0] = std.ascii.toUpper(next_character[0]);
+        }
+
+        if (std.mem.eql(u8, next_character, "-") or std.mem.eql(u8, next_character, "_")) {
+            next_character[0] = ' ';
+            capitalize = true;
+        }
+
+        try writer.print("{s}", .{next_character});
+    }
+}
+
+fn isPathIgnored(path: []const u8) bool {
+    var result: bool = false;
+    for (IGNORED_PATHS) |ignored_path| {
+        if (std.mem.eql(u8, path, ignored_path)) {
+            result = true;
+            break;
+        }
+    }
+    return result;
+}
+
+fn fileHasSubstitutions(path: []const u8) bool {
+    var result: bool = false;
+    for (FILES_WITH_SUBSTITUTIONS) |with_subs| {
+        if (std.mem.eql(u8, path, with_subs)) {
+            result = true;
+            break;
+        }
+    }
+    return result;
 }
