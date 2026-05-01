9ed9daf836c0223d51949d2e4a2451cd162d6d41
Zoee Silcock <mrzoee@gmail.com>
2026-05-01 11:42:16 +0200

    Rename DependecyType to DependencySet as it feels like a better description of what it defines

--- src/main.zig
+++ src/main.zig
@@ -186,7 +186,7 @@ pub fn main(init: std.process.Init) !void {
 
             if (code_changed) {
                 std.log.info("Code changed, rebuilding game library...", .{});
-                _ = try std.process.spawn(init.io, .{ .argv = &.{ "zig", "build", "-Dlib_only" } });
+                _ = try std.process.spawn(init.io, .{ .argv = &.{ "zig", "build", "-Dlib_only", "-fincremental" } });
             }
 
             if (dll_changed or assets_changed) {
