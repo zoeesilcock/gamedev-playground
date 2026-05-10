e946a1e0317976653081edeaaa964aa7b9c506ce
Zoee Silcock <mrzoee@gmail.com>
2026-05-10 12:10:52 +0200

    Update the integration docs to reflect the latest approach

--- src/lib/docs.zig
+++ src/lib/docs.zig
@@ -1,61 +1,108 @@
 //! This is the "flint" module that is exposed by flint which contains various building blocks that
 //! can be imported into your game to serve as a basis for your game engine.
 //!
-//! ## Build integration
+//! ## Quick start
+//! The easiest way to get started is to use the new project generator:
+//! ```
+//! git clone https://github.com/zoeesilcock/flint.git && cd flint
+//!
+//! zig build new -- ../my_new_project
+//!
+//! cd ../my_new_project && zig build run
+//! ```
+//!
+//! ## Manual build integration
 //! * Add flint as a dependency in your `build.zig.zon` file by running:
 //! ```
 //! zig fetch --save git+https://github.com/zoeesilcock/flint.git#v0.10.0
 //! ```
-//! * Add the following to your `build.zig` file:
+//! * Example `build.zig` file:
 //! ```
+//! const std = @import("std");
 //! const flint = @import("flint");
 //!
-//! const target = b.standardTargetOptions(.{});
-//! const optimize = b.standardOptimizeOption(.{});
+//! const TARGETS = [_]std.Target.Query{
+//!     .{ .cpu_arch = .x86_64, .os_tag = .windows, .abi = .gnu },
+//!     .{ .cpu_arch = .x86_64, .os_tag = .linux, .abi = .gnu },
+//!     // .{ .cpu_arch = .aarch64, .os_tag = .macos },
+//! };
+//! const OPTIMIZE_MODES = [_]std.builtin.OptimizeMode{ .Debug, .ReleaseFast };
+//! const INTERNAL_MODES = [_]bool{ true, false };
 //!
-//! // Integrate Flint.
-//! const result = flint.integrate(b, .{
-//!     .dependency = b.dependency("flint", .{ .target = target, .optimize = optimize }),
-//!     .target = target,
-//!     .optimize = optimize,
-//!     .build_options = b.addOptions(),
-//!     .name = b.option([]const u8, "name", "name of the shared library") orelse "your_lib_name",
-//!     .internal = b.option(bool, "internal", "include debug interface") orelse true,
-//!     .lib_only = b.option(bool, "lib_only", "only build the shared library") orelse false,
-//! });
+//! pub fn build(b: *std.Build) void {
+//!     const target = b.standardTargetOptions(.{});
+//!     const optimize = b.standardOptimizeOption(.{});
+//!     const name = b.option([]const u8, "name", "name of the game (used for exe and lib)") orelse "template";
+//!     const internal = b.option(bool, "internal", "include debug interface") orelse true;
+//!     const lib_only = b.option(bool, "lib_only", "only build the shared library") orelse false;
 //!
-//! // Game library.
-//! const module = b.createModule(.{
-//!     .root_source_file = b.path("src/root.zig"),
-//!     .target = target,
-//!     .optimize = optimize,
-//! });
-//! module.addImport("build_options", result.build_options_mod);
-//! module.addImport("flint", result.flint_mod);
+//!     // Build game.
+//!     const flint_options: flint.IntegrateOptions = .{
+//!         .dependency = b.dependency("flint", .{ .target = target, .optimize = optimize }),
+//!         .target = target,
+//!         .optimize = optimize,
+//!         .build_options = b.addOptions(),
+//!         .name = name,
+//!         .internal = internal,
+//!         .lib_only = lib_only,
+//!         .install_step = b.getInstallStep(),
+//!         .dest_dir = .default,
+//!     };
+//!     const result = buildGame(b, flint_options);
 //!
-//! // Build the game as a dynamic library.
-//! const lib = b.addLibrary(.{
-//!     .name = "your_lib_name",
-//!     .linkage = .dynamic,
-//!     .root_module = module,
-//! });
-//! b.getInstallStep().dependOn(&b.addInstallArtifact(lib, .{}).step);
+//!     // Build all variations.
+//!     const build_all_step = b.step("all", "Builds all permutations of the game for testing purposes.");
+//!     flint.buildMatrix(b, build_all_step, flint_options, &buildGame, &TARGETS, &OPTIMIZE_MODES, &INTERNAL_MODES);
 //!
-//! if (result.exe) |exe| {
-//!     b.getInstallStep().dependOn(&b.addInstallArtifact(exe, .{}).step);
+//!     // Install executable.
+//!     if (result.exe) |exe| {
+//!         b.getInstallStep().dependOn(&b.addInstallArtifact(exe, .{}).step);
+//!     }
+//!
+//!     // Install library.
+//!     b.getInstallStep().dependOn(&b.addInstallArtifact(result.lib, .{}).step);
+//!
+//!     // Check library.
+//!     const lib_check = b.addLibrary(.{
+//!         .name = name,
+//!         .linkage = .dynamic,
+//!         .root_module = result.lib.root_module,
+//!     });
+//!     const check = b.step("check", "Check if it compiles");
+//!     check.dependOn(&lib_check.step);
+//!
+//!     // Tests.
+//!     const test_step = b.step("test", "Run unit tests");
+//!     const lib_tests = b.addTest(.{ .root_module = result.lib.root_module });
+//!     const run_lib_tests = b.addRunArtifact(lib_tests);
+//!     test_step.dependOn(&run_lib_tests.step);
 //! }
-//! ```
-//! * The `build_options` field accepts a `*std.Build.Step.Options`. You can add your own
-//!   custom options to it before passing it to `integrate`. Flint will add its required
-//!   options (`internal` and `name`) and create the module for you:
-//! ```
-//! const build_options = b.addOptions();
-//! build_options.addOption(bool, "my_custom_flag", true);
 //!
-//! const result = flint.integrate(b, .{
-//!     // ...
-//!     .build_options = build_options,
-//! });
+//! fn buildGame(
+//!     b: *std.Build,
+//!     flint_options: flint.IntegrateOptions,
+//! ) flint.BuildResult {
+//!     // Integrate Flint.
+//!     const result = flint.integrate(b, flint_options);
+//!
+//!     // Game library.
+//!     const module = b.createModule(.{
+//!         .root_source_file = b.path("src/root.zig"),
+//!         .target = flint_options.target,
+//!         .optimize = flint_options.optimize,
+//!     });
+//!     module.addImport("build_options", result.build_options_mod);
+//!     module.addImport("flint", result.flint_mod);
+//!
+//!     const lib = b.addLibrary(.{
+//!         .name = flint_options.name,
+//!         .linkage = .dynamic,
+//!         .root_module = module,
+//!         .use_llvm = true,
+//!     });
+//!
+//!     return .{ .exe = result.exe, .lib = lib, .assets_path = b.path("assets") };
+//! }
 //! ```
 //!
 //! * The `internal` option decides if things like inspectors, editors, debug visualizations,
@@ -65,7 +112,7 @@
 //! const INTERNAL: bool = @import("build_options").internal;
 //! ```
 //!
-//! * See `src/examples/template/build.zig` for a complete example.
+//! * See `src/examples/template` for a complete example.
 //!
 //! ## Game library
 //! Flint expects you to create a library which exposes specific functions that will get called at various points in
